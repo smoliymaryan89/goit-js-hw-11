3e1e50dca7017b16524f648ad562e2ddbec7cb17
maryan <smoliymaryan89@gmail.com>
2023-06-16 20:33:21 +0300

    Update PixabayAPIService.js

--- src/js/PixabayAPIService.js
+++ src/js/PixabayAPIService.js
@@ -1,19 +1,28 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '37098100-c1f03ed35a5bcbe47235bac6b';
+export default class PixabayAPIService {
+  #BASE_URL = 'https://pixabay.com/api/';
+  #API_KEY = '37098100-c1f03ed35a5bcbe47235bac6b';
 
-export class PixabayAPIService {
   constructor() {
     this.page = 1;
     this.searchValue = '';
+    this.perPage = 40;
   }
 
   async getImages() {
-    const response = await axios.get(
-      `${BASE_URL}?key=${API_KEY}&q=${this.searchValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`
-    );
-    this.incrementPage();
+    const response = await axios.get(`${this.#BASE_URL}?`, {
+      params: {
+        key: this.#API_KEY,
+        page: this.page,
+        q: this.searchValue,
+        per_page: this.perPage,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    });
+    // this.incrementPage();
     return response.data;
   }
 
